aec36b889bd24a644b14eee8862f548ca733598e
Ed Lott <elott81@gmail.com>
2021-03-28 17:28:47 -0400

    added colors and formatting to layout

--- app.js
+++ app.js
@@ -2,18 +2,6 @@ const tickers = document.querySelectorAll('.ticker');
 const prices = document.querySelectorAll('.price');
 const changes = document.querySelectorAll('.change')
 
-// const getData = async function (){
-//     for(let ticker of tickers){
-//         const r = await fetch(`https://api.cryptonator.com/api/ticker/${ticker.textContent}-usd`);
-//         const priceData = await r.json();
-//         const num = parseFloat(priceData.ticker.price).toFixed(2);
-//         const price = num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-//         const change = priceData.ticker.change;
-//         const percent = ((change / num) * 100);
-//         ticker.nextElementSibling.textContent = `$${price}`;
-//         // square = `${percent.toFixed(2)}%`;
-//     }
-// }
 const getData = async function (){
     let urls = [];
     for (let ticker of tickers){
