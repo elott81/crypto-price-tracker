36647cc1fa4998c3be2432f1db7d27e0957dc62c
Ed Lott <elott81@gmail.com>
2021-03-27 20:53:22 -0400

    added percent change data to displays

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ const getData = async function (){
     for(let i = 0; i < priceData.length; i++){
         const num = parseFloat(priceData[i].ticker.price).toFixed(2);
         const price = num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-        prices[i].textContent = price;
+        prices[i].textContent = `$${price}`;
+        const change = priceData[i].ticker.change;
+        const percent = ((change / num) * 100);
+        changes[i].textContent = `${percent.toFixed(2)}%`;
     }
 }
 getData();
