3ce687b0eb254baa911912da04409cfedc504357
Ed Lott <elott81@gmail.com>
2021-03-27 20:44:52 -0400

    added data for top 20 cryptos to displays

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const squares = document.querySelectorAll('.square')
+const tickers = document.querySelectorAll('.ticker');
+const prices = document.querySelectorAll('.price');
+const changes = document.querySelectorAll('.change')
 
+// const getData = async function (){
+//     for(let ticker of tickers){
+//         const r = await fetch(`https://api.cryptonator.com/api/ticker/${ticker.textContent}-usd`);
+//         const priceData = await r.json();
+//         const num = parseFloat(priceData.ticker.price).toFixed(2);
+//         const price = num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+//         const change = priceData.ticker.change;
+//         const percent = ((change / num) * 100);
+//         ticker.nextElementSibling.textContent = `$${price}`;
+//         // square = `${percent.toFixed(2)}%`;
+//     }
+// }
 const getData = async function (){
-    const r = await fetch(`https://api.cryptonator.com/api/ticker/btc-usd`);
-    const priceData = await r.json();
-    console.log(priceData)
-    const ticker = priceData.ticker.base;
-    const price = parseFloat(priceData.ticker.price).toFixed(2)
-    const change = priceData.ticker.change;
-    const percent = ((change / price) * 100);
-    console.log(percent.toFixed(2));
-    for(let square of squares){
-        square.childNodes[1].textContent = `${ticker}`;
-        square.childNodes[3].textContent = `${price}`;
-        square.childNodes[5].textContent = `${percent.toFixed(2)}`;
+    let urls = [];
+    for (let ticker of tickers){
+        urls.push(`https://api.cryptonator.com/api/ticker/${ticker.textContent}-usd`)
+    }
+    let priceData = await Promise.all(
+        urls.map(async url => {
+                    let response = await fetch(url);
+                    return response.json();
+                })
+            );
+    for(let i = 0; i < priceData.length; i++){
+        const num = parseFloat(priceData[i].ticker.price).toFixed(2);
+        const price = num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        prices[i].textContent = price;
     }
 }
-
 getData();
